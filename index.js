6fa2db837883d91ba3bc2911e544b984024a58a3
Duishenalieva <liltril783@gmail.com>
2024-01-11 00:56:31 +0600

    task: encrypt and decrypt text done

--- index.js
+++ index.js
@@ -2,6 +2,18 @@ const express = require("express");
 const app = express();
 const port = 8000;
 
+const Vigenere = require("caesar-salad").Vigenere;
+
+app.get("/encode/:cipher", (req, res) => {
+  const result = Vigenere.Cipher("password").crypt(req.params.cipher);
+  res.send(`<h1>${result}</h1>`);
+});
+
+app.get("/decode/:cipher", (req, res) => {
+  const result = Vigenere.Decipher("password").crypt(req.params.cipher);
+  res.send(`<h1>${result}</h1>`);
+});
+
 app.get("/:echo", (req, res) => {
   res.send(`<h1>${req.params.echo}</h1>`);
 });
