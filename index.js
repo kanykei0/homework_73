afe388cc1ee2427e2cd66c01f16e2898c0c368e4
Duishenalieva <liltril783@gmail.com>
2024-01-11 01:01:20 +0600

    added password for vigenere cipher

--- index.js
+++ index.js
@@ -3,14 +3,15 @@ const app = express();
 const port = 8000;
 
 const Vigenere = require("caesar-salad").Vigenere;
+const password = "password";
 
 app.get("/encode/:cipher", (req, res) => {
-  const result = Vigenere.Cipher("password").crypt(req.params.cipher);
+  const result = Vigenere.Cipher(password).crypt(req.params.cipher);
   res.send(`<h1>${result}</h1>`);
 });
 
 app.get("/decode/:cipher", (req, res) => {
-  const result = Vigenere.Decipher("password").crypt(req.params.cipher);
+  const result = Vigenere.Decipher(password).crypt(req.params.cipher);
   res.send(`<h1>${result}</h1>`);
 });
 
